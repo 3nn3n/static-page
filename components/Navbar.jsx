4a21a02e387b1eca9efc669fe1fa7e4b9c0f0d0b
Nitesh Negi <nitesh_negi@outlook.com>
2024-12-20 16:45:42 +0530

    second commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
       <header>
           <nav>
-              <img src="../R.png" alt="Apple logo" />
+              <img src="/static-page/R.png" alt="Apple logo" />
               <span>AppleFacts</span>
           </nav>
       </header>
